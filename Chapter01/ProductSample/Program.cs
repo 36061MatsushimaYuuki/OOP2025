c5974804a48f0b85452299b17334ef2e98b43451
infosys <infosys@C4209>
2025-04-21 09:57:49 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product ramen = new Product(240, "ラーメン", 300);
+
+            // 税抜きの価格を表示【かりんとうの税抜き価格は○○円です】
+            Console.WriteLine(karinto.Name + "の税抜き価格は" + karinto.Price + "円です");
+
+            // 消費税額の表示【かりんとうの消費税額は○○円です】
+            Console.WriteLine(karinto.Name + "の消費税額は" + karinto.GetTax() + "円です");
+
+            // 税込価格の表示【かりんとうの税込価格は○○円です】
+            Console.WriteLine(karinto.Name + "の税込価格は" + karinto.GetPriceIncludingTax() + "円です");
+
+            Console.WriteLine(ramen.Name + "の税抜き価格は" + ramen.Price + "円です");
+
+            Console.WriteLine(ramen.Name + "の消費税額は" + ramen.GetTax() + "円です");
+
+            Console.WriteLine(ramen.Name + "の税込価格は" + ramen.GetPriceIncludingTax() + "円です");
+        }
+    }
+}
---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // デフォルト用の値を設定
+            int def_start = 1;
+            int def_end = 10;
+            // 引数が渡されていない場合、配列から参照しない
+            // 例：-tom 1 10 => true / -tom => false
+            int start = args.Length > 1 ? int.Parse(args[1]) : def_start;
+            int end = args.Length > 2 ? int.Parse(args[2]) : def_end;
+            // startがendより大きい場合入れ替え
+            int[] result_nums = ChangeMinMax(start, end);
+            start = result_nums[0];
+            end = result_nums[1];
+
+            if (args.Length > 0 && args[0].Equals("-tom")) {
+                PrintFeetToMeterList(start, end);
+            } else {
+                PrintMeterToFeetList(start, end);
+            }
+        }
+
+        static int[] ChangeMinMax(int min, int max) {
+            int[] result = {min, max};
+            if (min > max) {
+                int work = min;
+                min = max;
+                max = work;
+                result[0] = min;
+                result[1] = max;
+            }
+            return result;
+        }
+
+        // フィートからメートルへの対応表を出力
+        static void PrintFeetToMeterList(int start, int end) {
+            for (int feet = start; feet <= end; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet} ft = {meter:0.0000} m");
+            }
+        }
+
+        // メートルからフィートへの対応表を出力
+        static void PrintMeterToFeetList(int start, int end) {
+            for (int meter = start; meter <= end; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter} m = {feet:0.0000} ft");
+            }
+        }
+
+        // フィートからメートルへの変換
+        static double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+
+        // メートルからフィートへの変換
+        static double MeterToFeet(int meter) {
+            return meter / 0.3048;
+        }
+    }
+}
