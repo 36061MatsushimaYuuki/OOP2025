c905d1904503e4c58a49bba8f0da5a0f54457eef
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-04-22 12:05:47 +0900

    コンソール入力に対応

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -1,16 +1,47 @@
-﻿namespace Exercise02 {
+﻿using System.Text;
+
+namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
 
-            PrintInchToMeterList(1, 10);
-            PrintMeterToInchList(1, 10);
+            Console.WriteLine("***　変換アプリ　***");
+            Console.WriteLine("1：インチからメートル");
+            Console.WriteLine("2：メートルからインチ");
+            Console.Write("＞");
+            // モード選択
+            int mode = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if(mode == 1) { // インチからメートル
+                int[] getParams = SetStartEnd();
+                PrintInchToMeterList(getParams[0], getParams[1]);
+            } else if (mode == 2) { // メートルからインチ
+                int[] getParams = SetStartEnd();
+                PrintMeterToInchList(getParams[0], getParams[1]);
+            } else {
+                Console.WriteLine("エラー：値が不正です");
+            }
+        }
+
+        static int[] SetStartEnd() {
+            int[] result = {1, 1};
+            for (int i = 0; i < 2; i++) {
+                if(i % 2 == 0) {
+                    Console.Write("はじめ：");
+                } else {
+                    Console.Write("おわり：");
+                }
+                int num = int.Parse(Console.ReadLine());
+                result[i] = num;
+            }
+            return result;
         }
 
         // メートルからインチへの対応表を出力
         static void PrintMeterToInchList(int start, int end) {
             for (int meter = start; meter <= end; meter++) {
                 double inch = InchConverter.ToInch(meter);
-                Console.WriteLine($"{meter} m = {inch:0.0000} in");
+                Console.WriteLine($"{meter} m = {inch:0.0000} inch");
             }
         }
 
@@ -18,7 +49,7 @@
         static void PrintInchToMeterList(int start, int end) {
             for (int inch = start; inch <= end; inch++) {
                 double meter = InchConverter.ToMeter(inch);
-                Console.WriteLine($"{inch} in = {meter:0.0000} m");
+                Console.WriteLine($"{inch} inch = {meter:0.0000} m");
             }
         }
     }
