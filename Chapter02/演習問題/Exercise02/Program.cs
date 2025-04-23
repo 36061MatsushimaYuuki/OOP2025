d744bf2b905bf652d3617672341c0ddcfc05929c
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-04-23 10:19:50 +0900

    機能追加②完成

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -4,6 +4,7 @@ namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
 
+#if false
             Console.WriteLine("***　変換アプリ　***");
             Console.WriteLine("1：インチからメートル");
             Console.WriteLine("2：メートルからインチ");
@@ -21,12 +22,45 @@ namespace Exercise02 {
             } else {
                 Console.WriteLine("エラー：値が不正です");
             }
+#else
+            Console.WriteLine("***　変換アプリ　***");
+            Console.WriteLine("1：ヤードからメートル");
+            Console.WriteLine("2：メートルからヤード");
+            Console.Write("＞");
+            // モード選択
+            int mode = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (mode == 1) { // ヤードからメートル
+                Console.Write("変換前(ヤード): ");
+                int yard = int.Parse(Console.ReadLine());
+                PrintYardToMeter(yard);
+            } else if (mode == 2) { // メートルからヤード
+                Console.Write("変換前(メートル): ");
+                int meter = int.Parse(Console.ReadLine());
+                PrintMeterToYard(meter);
+            } else {
+                Console.WriteLine("エラー：値が不正です");
+            }
+#endif
+        }
+
+        // ヤードからメートルへ変換
+        static void PrintYardToMeter(int yard) {
+            double meter = YardConverter.ToMeter(yard);
+            Console.WriteLine($"変換後(メートル): {meter:0.000}");
+        }
+
+        // メートルからヤードへ変換
+        static void PrintMeterToYard(int meter) {
+            double yard = YardConverter.ToYard(meter);
+            Console.WriteLine($"変換後(ヤード): {yard:0.000}");
         }
 
         static int[] SetStartEnd() {
-            int[] result = {1, 1};
+            int[] result = { 1, 1 };
             for (int i = 0; i < 2; i++) {
-                if(i % 2 == 0) {
+                if (i % 2 == 0) {
                     Console.Write("はじめ：");
                 } else {
                     Console.Write("おわり：");
@@ -34,7 +68,7 @@ namespace Exercise02 {
                 int num = int.Parse(Console.ReadLine());
                 result[i] = num;
             }
-            if (result[0] > result[1]) {
+            if (result[0] > result[1]) { // 大, 小の関係のとき入替
                 int work = result[0];
                 result[0] = result[1];
                 result[1] = work;
---
+++ Chapter02/演習問題/Exercise02/YardConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    public static class YardConverter {
+
+        // 定数
+        private const double ratio = 1.094;
+
+        // ヤードからメートルを求める
+        public static double ToMeter(double yard) {
+            return yard / ratio;
+        }
+
+        // メートルからヤードを求める
+        public static double ToYard(double meter) {
+            return meter * ratio;
+        }
+    }
+}
