b9e00910ae62e8817a71fae34bb1bf105ea210cc
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-05-07 09:36:49 +0900

    FormApps追加

---
+++ FormApps/StopWatch/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace StopWatch {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.lbTimeDisp = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.btReset = new System.Windows.Forms.Button();
+            this.tmDispTimer = new System.Windows.Forms.Timer(this.components);
+            this.lbLap = new System.Windows.Forms.ListBox();
+            this.btLap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbTimeDisp
+            // 
+            this.lbTimeDisp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lbTimeDisp.Font = new System.Drawing.Font("游明朝", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTimeDisp.Location = new System.Drawing.Point(38, 36);
+            this.lbTimeDisp.Name = "lbTimeDisp";
+            this.lbTimeDisp.Size = new System.Drawing.Size(339, 53);
+            this.lbTimeDisp.TabIndex = 0;
+            // 
+            // btStart
+            // 
+            this.btStart.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btStart.Location = new System.Drawing.Point(45, 126);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(137, 49);
+            this.btStart.TabIndex = 1;
+            this.btStart.Text = "スタート";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btStop.Location = new System.Drawing.Point(45, 200);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(137, 49);
+            this.btStop.TabIndex = 1;
+            this.btStop.Text = "ストップ";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // btReset
+            // 
+            this.btReset.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btReset.Location = new System.Drawing.Point(240, 126);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(137, 49);
+            this.btReset.TabIndex = 1;
+            this.btReset.Text = "リセット";
+            this.btReset.UseVisualStyleBackColor = true;
+            this.btReset.Click += new System.EventHandler(this.btReset_Click);
+            // 
+            // tmDispTimer
+            // 
+            this.tmDispTimer.Tick += new System.EventHandler(this.tmDispTimer_Tick);
+            // 
+            // lbLap
+            // 
+            this.lbLap.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbLap.FormattingEnabled = true;
+            this.lbLap.ItemHeight = 29;
+            this.lbLap.Location = new System.Drawing.Point(420, 36);
+            this.lbLap.Name = "lbLap";
+            this.lbLap.Size = new System.Drawing.Size(279, 265);
+            this.lbLap.TabIndex = 2;
+            // 
+            // btLap
+            // 
+            this.btLap.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btLap.Location = new System.Drawing.Point(240, 200);
+            this.btLap.Name = "btLap";
+            this.btLap.Size = new System.Drawing.Size(137, 49);
+            this.btLap.TabIndex = 1;
+            this.btLap.Text = "ラップ→";
+            this.btLap.UseVisualStyleBackColor = true;
+            this.btLap.Click += new System.EventHandler(this.btLap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(748, 373);
+            this.Controls.Add(this.lbLap);
+            this.Controls.Add(this.btReset);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btLap);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.lbTimeDisp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTimeDisp;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Button btReset;
+        private System.Windows.Forms.Timer tmDispTimer;
+        private System.Windows.Forms.ListBox lbLap;
+        private System.Windows.Forms.Button btLap;
+    }
+}
+
---
+++ FormApps/UnitConverter/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnitConverter
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btChange_Click(object sender, EventArgs e) {
+            int num1;
+            if (int.TryParse(tbNum1.Text, out num1)) {
+                double num2 = num1 * 0.3048;
+                tbNum2.Text = num2.ToString();
+            } else {
+                DialogResult result = MessageBox.Show("値が入力されていないか、形式が正しくありません。", "", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+
+        private void btCalc_Click(object sender, EventArgs e) {
+            if(nudNum2.Value != 0) {
+                nudAnswer.Value = Math.Floor(nudNum1.Value / nudNum2.Value);
+                nudAmari.Value = nudNum1.Value % nudNum2.Value;
+            } else {
+                DialogResult result = MessageBox.Show("0で除算はできません。", "", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+    }
+}
