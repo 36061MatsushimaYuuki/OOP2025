b484a1fa3e375e079dd7115c7f90274683ec97ff
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-05-13 10:24:37 +0900

    2-1-3 完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    // 2-1-1
+    public class Song {
+
+        /* プロパティ */
+        public string Title { get; set; } = String.Empty;
+
+        public string ArtistName { get; set; } = String.Empty;
+
+        public int Length { get; set; }
+
+        // 2-1-2
+        public Song(string title, string artistName, int length) {
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
+        }
+    }
+}
