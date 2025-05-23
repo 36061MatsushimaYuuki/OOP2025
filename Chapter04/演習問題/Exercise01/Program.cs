79a9263e472a665a1edff025cc58c715c7a1463f
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-05-23 14:02:29 +0900

    4.1.3まで完成

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -16,15 +16,38 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-            langs.Where(s => s.Contains('s'));
+            // foreach
+            foreach (var lang in langs) {
+                if (lang.Contains('S'))
+                    Console.WriteLine(lang);
+            }
+            Console.WriteLine();
+
+            // for
+            for (var cnt = 0; cnt < langs.Count; cnt++) {
+                if (langs[cnt].Contains('S')) {
+                    Console.WriteLine(langs[cnt]);
+                }
+            }
+            Console.WriteLine();
+
+            // while
+            var cnt2 = 0;
+            while (cnt2 < langs.Count) {
+                if (langs[cnt2].Contains('S')) {
+                    Console.WriteLine(langs[cnt2]);
+                }
+                cnt2++;
+            }
         }
 
         private static void Exercise2(List<string> langs) {
-
+            langs.FindAll(s => s.Contains('S')).ForEach(s => Console.WriteLine(s));
         }
 
         private static void Exercise3(List<string> langs) {
-
+            var lang = langs.Find(s => s.Length == 10) ?? "unknown";
+            Console.WriteLine(lang);
         }
     }
 }
