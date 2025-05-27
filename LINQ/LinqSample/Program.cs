7d33a4c4fb846d6629b1b0c39a6ea34d2bd6dcba
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-05-27 11:00:43 +0900

    7.1.4まで完成

---
+++ LINQ/LinqSample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinqSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = Enumerable.Range(1, 100);
+
+            // 8の倍数の合計を出力
+            Console.WriteLine("---8の倍数の合計を出力---");
+            Console.WriteLine(numbers.Where(n => n % 8 == 0).Sum());
+
+            //foreach(var num in numbers) {
+            //    Console.WriteLine(num);
+            //}
+        }
+    }
+}
