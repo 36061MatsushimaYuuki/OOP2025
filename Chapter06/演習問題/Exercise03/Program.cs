5e9b03607809c205ee5f61b4f9ae3d5b009ca291
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-06-04 10:07:30 +0900

    Chapter6.3.99 (コメントアウト)

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -28,17 +28,20 @@ namespace Exercise03 {
             #endregion
         }
 
+        //空白の数を数えて表示
         private static void Exercise1(string text) {
             var spaces = text.Count(c => c == ' ');
             //var spaces = text.Count(char.IsWhiteSpace);//別解
             Console.WriteLine("空白数:{0}", spaces);
         }
 
+        //bigをsmallに置き換えて表示
         private static void Exercise2(string text) {
             var replaced = text.Replace("big", "small");
             Console.WriteLine(replaced);
         }
 
+        //一旦単語別に分解し、StringBuilderで文字列を連結して表示
         private static void Exercise3(string text) {
             var array = text.Split(' ');
             var sb = new StringBuilder(array[0]);
@@ -50,11 +53,13 @@ namespace Exercise03 {
             Console.WriteLine(sb + ".");
         }
 
+        //文字列を単語に分けて単語数を表示
         private static void Exercise4(string text) {
             var count = text.Split(' ').Length;
             Console.WriteLine("単語数:{0}", count);
         }
 
+        //4文字以下の単語を表示
         private static void Exercise5(string text) {
             var words = text.Split(' ').Where(s => s.Length <= 4);
 
@@ -62,6 +67,7 @@ namespace Exercise03 {
                 Console.WriteLine(word);
         }
 
+        //アルファベットの数をカウントして表示する
         private static void Exercise6(string text) {
             for (var alphabet = 'a'; alphabet <= 'z'; alphabet++) {
                 Console.WriteLine($"{alphabet}: {text.Count(c => c == alphabet)}");
