69ae09e83d6542350db3cfa3848c98521ae4960a
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-06-06 14:49:08 +0900

    5.1.3完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -13,6 +13,13 @@ namespace Exercise01
 
         public bool Is21Century => _year > 2000 && _year <= 2100;
 
+        public YearMonth AddOneMonth() {
+            bool isLastMonth = _year == 12;
+            int calcYear = isLastMonth ? _year + 1 : _year;
+            int calcMonth = isLastMonth ? 1 : _month + 1;
+            return new YearMonth(calcYear, calcMonth);
+        }
+
         public YearMonth(int year, int month) {
             _year = year;
             _month = month;
