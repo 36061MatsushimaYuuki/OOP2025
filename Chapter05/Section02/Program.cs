f9ee3f4312c45ac05971dc225e2bbc81e9556961
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-06-06 14:43:40 +0900

    5.1.1完成

---
+++ Chapter05/Section02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var appVer1 = new AppVersion(5, 1);
+            var appVer2 = new AppVersion(5, 1);
+
+            if(appVer1 == appVer2) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+
+    // オプション引数を使った定義
+    public record AppVersion(int major, int minor, int build = 0, int revision = 0) {
+        // プライマリーコンストラクタを使った定義
+        public int Major { get; init; } = major;
+        public int Minor { get; init; } = minor;
+        public int Build { get; init; } = build;
+        public int Revision { get; init; } = revision;
+
+        public override string ToString() =>
+            $"{Major}.{Minor}.{Build}.{Revision}";
+
+    }
+}
---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    public class YearMonth
+    {
+        private readonly int _year;
+        private readonly int _month;
+
+        public YearMonth(int year, int month) {
+            _year = year;
+            _month = month;
+        }
+    }
+}
