84227589a0e6baffda03b53dc0c43a8a50b3d239
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-06-17 11:08:00 +0900

    設問1_Student クラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        //学生の名前
+        public string Name { get; init; } = string.Empty;
+        //科目名
+        public string Subject { get; init; } = string.Empty;
+        //点数
+        public int Score { get; init; }
+    }
+}
