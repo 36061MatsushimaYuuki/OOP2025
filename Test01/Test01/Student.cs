a51b706313f30a440f4829c3962a27885a2fe57a
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-06-17 11:50:14 +0900

    設問１_Student クラス完成

--- Test01/Test01/Student.cs
+++ Test01/Test01/Student.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 namespace Test01 {
     public class Student {
         //学生の名前
-        public string Name { get; init; } = string.Empty;
+        public required string Name { get; init; } = string.Empty;
         //科目名
-        public string Subject { get; init; } = string.Empty;
+        public required string Subject { get; init; } = string.Empty;
         //点数
-        public int Score { get; init; }
+        public required int Score { get; init; }
     }
 }
