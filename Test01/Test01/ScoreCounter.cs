d615eb666c4964f7d54d25b6fa21d28b640bb4eb
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-06-17 11:19:22 +0900

    設問１_提出

--- Test01/Test01/ScoreCounter.cs
+++ Test01/Test01/ScoreCounter.cs
@@ -4,26 +4,36 @@
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
+            _score = ReadScore(filePath);
         }
 
-        //メソッドの概要： 
+        //メソッドの概要： ファイルを読み込む
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+            var scores = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach(var line in lines) {
+                string[] items = line.Split(',');
+                var score = new Student() {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2]),
+                };
+                scores.Add(score);
+            }
 
-
-
-
+            return scores;
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-            
-
-
-
-
+            var dict = new Dictionary<string, int>();
+            foreach(var score in _score) {
+                if(dict.ContainsKey(score.Subject))
+                    dict[score.Subject] += score.Score;
+                else
+                    dict[score.Subject] = score.Score;
+            }
+            return dict;
         }
     }
 }
