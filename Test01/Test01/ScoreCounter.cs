7bf23edab78e2810b154eed7ef405dba3bce5b91
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-06-17 11:41:32 +0900

    設問１_コメント入力

--- Test01/Test01/ScoreCounter.cs
+++ Test01/Test01/ScoreCounter.cs
@@ -9,10 +9,13 @@
 
         //メソッドの概要： 生徒のテストデータを読み込み、Studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
+            //生徒のテストデータを入れるリストオブジェクトを生成
             var scores = new List<Student>();
             var lines = File.ReadAllLines(filePath);
+            //読み込んだ行数分繰り返す
             foreach(var line in lines) {
                 string[] items = line.Split(',');
+                //Studentオブジェクトを生成
                 var score = new Student() {
                     Name = items[0],
                     Subject = items[1],
@@ -28,6 +31,7 @@
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
             foreach(var score in _score) {
+                //辞書に科目の点数データがあれば合計、なければ新しく追加する
                 if(dict.ContainsKey(score.Subject))
                     dict[score.Subject] += score.Score;
                 else
