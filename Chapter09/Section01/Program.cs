d4dc7ca311fb7ae70cd577885e97bd10042ddd3e
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-06-24 15:03:05 +0900

    Chapter09演習問題

---
+++ Chapter09/Section01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var today = new DateTime(2025, 7, 12); //日付
+            var now = DateTime.Now;     //日付と時刻
+
+            //Console.WriteLine($"Today:{today.Month}");
+            //Console.WriteLine($"Now:{now}");
+
+            //自分の生年月日は何曜日かをプログラムを書いて調べる
+            Console.WriteLine("日付を入力");
+            Console.Write("西暦: ");
+            var year = int.Parse(Console.ReadLine());
+            Console.Write("月: ");
+            var month = int.Parse(Console.ReadLine());
+            Console.Write("日: ");
+            var day = int.Parse(Console.ReadLine());
+
+            var birthday = new DateTime(year, month, day);
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var japaneseDate = birthday.ToString("ggyy年M月d日", culture); //和暦変換
+            var weekday = culture.DateTimeFormat.GetDayName(birthday.DayOfWeek);
+
+            //日数の計算
+            var livingDate = now.Date - birthday.Date;
+
+            Console.WriteLine($"{japaneseDate}は{weekday}です。");
+            Console.WriteLine($"生まれてから{livingDate.Days}日目です");
+
+            //うるう年の判定プログラムを作成する
+            Console.WriteLine();
+            Console.WriteLine("閏年判定プログラム");
+            Console.Write("西暦を入力: ");
+            var checkyear = int.Parse(Console.ReadLine());
+
+            var message = DateTime.IsLeapYear(checkyear) ? "閏年" : "平年" ;
+            Console.WriteLine($"{checkyear}年は{message}です");
+
+            Console.WriteLine();
+            Console.WriteLine("現在の時刻");
+            while (true) {
+                var nowTime = DateTime.Now;
+                Console.Write($"\r{nowTime}");
+            }
+        }
+    }
+}
