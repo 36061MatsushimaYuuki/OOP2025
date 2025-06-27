b8630628013327dc722077b8e7231bc760f8dbdc
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-06-27 11:26:52 +0900

    演習問題9-3完成（9-2改善）

--- Chapter09/演習問題/Exercise02/Program.cs
+++ Chapter09/演習問題/Exercise02/Program.cs
@@ -28,11 +28,9 @@ namespace Exercise02 {
 
         // 9.2.1【ここにプログラムを作成する】
         static DateTime NextWeek(DateTime date, DayOfWeek dayOfWeek) {
-            //あと何日で次の週か求める
-            var addDays = 7 - (int)date.DayOfWeek;
-            var nextweek = date.AddDays(addDays);
-            var days = (int)dayOfWeek; //求める曜日を足す
-            return nextweek.AddDays(days);
+            //あと何日で次の週か求め、求める曜日を足す
+            var addDays = 7 - (int)date.DayOfWeek + (int)dayOfWeek;
+            return date.AddDays(addDays);
         }
 
         private static void Exercise2() {
