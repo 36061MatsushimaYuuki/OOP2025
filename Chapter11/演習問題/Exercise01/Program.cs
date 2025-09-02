bf3ffb989e1e672725108c3fbb62c8ef55ef70b2
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-09-02 13:23:12 +0900

    11章まで終了

---
+++ Chapter11/演習問題/Exercise01/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(IsPhoneNumber("080-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("060-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("190-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("091-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-9111-12341"));
+            Console.WriteLine(IsPhoneNumber("A090-9111-1234"));
+            Console.WriteLine(IsPhoneNumber("090-911-1234"));
+            Console.WriteLine(IsPhoneNumber("090-1911-234"));
+            //追加分
+            Console.WriteLine("-------------");
+            Console.WriteLine(IsPhoneNumber("070-1911-234D"));
+            Console.WriteLine(IsPhoneNumber("070-19A1-2342"));
+            Console.WriteLine(IsPhoneNumber("070-19111-2341"));
+        }
+
+        private static bool IsPhoneNumber(string telNum) {
+            return Regex.IsMatch(telNum, @"^0[7-9]0-\d{4}-\d{4}$");
+        }
+    }
+}
---
+++ Chapter11/演習問題/Exercise02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            string filePath = "sample.txt";
+            Pickup3DigitNumber(filePath);
+            Console.WriteLine("------------");
+            PickupAlphabet(filePath);
+        }
+
+        private static void Pickup3DigitNumber(string filePath) {
+            foreach(var line in File.ReadLines(filePath)) {
+                var matches = Regex.Matches(line, @"\b\d{3,}\b");
+                foreach(Match m in matches) {
+                    //結果を出力
+                    Console.WriteLine(m.Value);
+                }
+            }
+        }
+
+        private static void PickupAlphabet(string filePath) {
+            foreach (var line in File.ReadLines(filePath)) {
+                var matches = Regex.Matches(line, @"\b[a-zA-Z]+\b");
+                foreach (Match m in matches) {
+                    //結果を出力
+                    Console.WriteLine(m.Value);
+                }
+            }
+        }
+    }
+}
