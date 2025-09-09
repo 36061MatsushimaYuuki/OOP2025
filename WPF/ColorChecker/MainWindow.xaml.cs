3ff6e1e7eb51873a6807a46d4bb32194415ac43a
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-09-09 14:43:28 +0900

    WPF ColorChecker完成

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -24,12 +24,64 @@ namespace ColorChecker
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = GetColorList();
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
             colorArea.Background = new SolidColorBrush(
                 Color.FromRgb((byte)RedSlider.Value, (byte)GreenSlider.Value, (byte)BlueSlider.Value)
             );
+        }
+
+        private void Stock_Button_Click(object sender, RoutedEventArgs e) {
+            var r = (byte)RedSlider.Value;
+            var g = (byte)GreenSlider.Value;
+            var b = (byte)BlueSlider.Value;
+            var thisColor = Color.FromRgb(r, g, b);
+            //comboBoxが選択されていない状態でも調べる
+            var equalItem = ColorSelect_ComboBox.Items.Cast<MyColor>().FirstOrDefault(c => c.Color == thisColor);
+            string colorName = null;
+            if(equalItem != null) {
+                colorName = equalItem.Name;
+            }
+            Stock_List.Items.Add(new MyColor {
+                Color = thisColor,
+                Name = colorName ?? "R: " + r + " G: " + g + " B: " + b
+            });
+        }
+
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+
+        private void ColorSelect_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if(ColorSelect_ComboBox.SelectedItem == null) {
+                return;
+            }
+            var selectedItem = (MyColor)ColorSelect_ComboBox.SelectedItem;
+            colorArea.Background = new SolidColorBrush(
+                selectedItem.Color
+            );
+            RedSlider.Value = selectedItem.Color.R;
+            GreenSlider.Value = selectedItem.Color.G;
+            BlueSlider.Value = selectedItem.Color.B;
+        }
+
+        private void Stock_List_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if(Stock_List.SelectedItem == null) {
+                return;
+            }
+            var selectedItem = (MyColor)Stock_List.SelectedItem;
+            colorArea.Background = new SolidColorBrush(
+                selectedItem.Color
+            );
+            RedSlider.Value = selectedItem.Color.R;
+            GreenSlider.Value = selectedItem.Color.G;
+            BlueSlider.Value = selectedItem.Color.B;
+            //セレクト状態をリセット
+            Stock_List.SelectedIndex = -1;
         }
     }
 }
