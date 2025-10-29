68e145255a0e9a3e622447871ad78b4260d36174
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-10-29 10:00:14 +0900

    演習問題６完成

--- Chapter13/演習問題/Exercise01/Program.cs
+++ Chapter13/演習問題/Exercise01/Program.cs
@@ -61,6 +61,22 @@ namespace Exercise01 {
 
         private static void Exercise1_6() {
             Console.WriteLine("[6] -");
+            var groups = Library.Categories
+                .GroupJoin(Library.Books,
+                    c => c.Id,
+                    b => b.CategoryId,
+                    (c, books) => new {
+                        Category = c.Name,
+                        Books = books
+                    }
+                )
+                .OrderBy(c => c.Category);
+            foreach (var group in groups) {
+                Console.WriteLine(group.Category);
+                foreach (var book in group.Books) {
+                    Console.WriteLine($"  {book.Title}");
+                }
+            }
         }
 
         private static void Exercise1_7() {
