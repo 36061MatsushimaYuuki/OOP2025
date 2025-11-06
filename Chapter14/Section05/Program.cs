7e7929d0cbbb3fbb7a6ebd92e36123c2de523b3a
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-11-06 15:55:12 +0900

    コード14.20

--- Chapter14/Section05/Program.cs
+++ Chapter14/Section05/Program.cs
@@ -1,14 +1,58 @@
-﻿namespace Section05 {
-    internal class Program {
-        static void Main(string[] args) {
-            var selected = Library.Books
-                .AsParallel()
-                .AsOrdered()
-                .Where(b => b.Price > 500 && b.Price < 2000)
-                .Select(b => new { b.Title });
+﻿using System.Diagnostics;
 
-            foreach(var item in selected) {
-                Console.WriteLine(item.Title);
+namespace Section05 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            await Code14_20.Run();
+        }
+    }
+
+    public class Code14_20 {
+
+        public static async Task Run() {
+            var sw = Stopwatch.StartNew();
+            var task1 = Task.Run(() => Get5000thPrime());
+            var task2 = Task.Run(() => Get6000thPrime());
+            var prime1 = await task1;
+            var prime2 = await task2;
+            sw.Stop();
+            Console.WriteLine(prime1);
+            Console.WriteLine(prime2);
+            Console.WriteLine($"実行時間: {sw.ElapsedMilliseconds}ミリ秒");
+        }
+
+        public static void Run2() {
+            var sw = Stopwatch.StartNew();
+            var prime1 = Get5000thPrime();
+            var prime2 = Get6000thPrime();
+            sw.Stop();
+            Console.WriteLine(prime1);
+            Console.WriteLine(prime2);
+            Console.WriteLine($"実行時間: {sw.ElapsedMilliseconds}ミリ秒");
+        }
+        // 5000番目の素数を求める
+        private static int Get5000thPrime() {
+            return GetPrimes().Skip(4999).First();
+        }
+
+        // 6000番目の素数を求める
+        private static int Get6000thPrime() {
+            return GetPrimes().Skip(5999).First();
+        }
+
+        // 上記2つのメソッドから呼び出される下位メソッド
+        // あえて効率の悪いアルゴリズムで記述している
+        public static IEnumerable<int> GetPrimes() {
+            for (int i = 2; i < int.MaxValue; i++) {
+                bool isPrime = true;
+                for (int j = 2; j < i; j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                    yield return i;
             }
         }
     }
