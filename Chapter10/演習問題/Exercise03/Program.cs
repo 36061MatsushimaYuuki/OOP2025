6c0a9f694f57f366df02e7751baad77a9e2a192d
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-11-10 15:02:42 +0900

    演習問題10.3完成

--- Chapter10/演習問題/Exercise03/Program.cs
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Exercise03 {
+﻿using System.Text;
+
+namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("ファイル1のパスを入力:");
+            var filePath1 = Console.ReadLine();
+            Console.WriteLine("ファイル2のパスを入力:");
+            var filePath2 = Console.ReadLine();
+            if(!File.Exists(filePath1) || !File.Exists(filePath2)) {
+                return;
+            }
+
+            using (var writer = new StreamWriter(filePath1, append: true)) {
+                using (var reader = new StreamReader(filePath2, Encoding.UTF8)) {
+                    var texts = reader.ReadToEnd();
+                    writer.WriteLine();
+                    writer.WriteLine(texts);
+                }
+            }
         }
     }
 }
