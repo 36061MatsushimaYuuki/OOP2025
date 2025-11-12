d790bc9604286512f5549cba574fcf9f12ef1477
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-11-12 12:05:50 +0900

    WordCounter完成

---
+++ Chapter15/TemplateMethod/WordCounter/Program.cs
@@ -0,0 +1,21 @@
+﻿using TextFileProcessor;
+using WordCounter;
+
+namespace PublicCounter {
+    internal class Program {
+        static void Main(string[] args) {
+            string? filePath = "";
+            while (string.IsNullOrWhiteSpace(filePath)) {
+                Console.Write("ファイルパスを入力: ");
+                filePath = Console.ReadLine();
+            }
+            string? word = "";
+            while (string.IsNullOrWhiteSpace(word)) {
+                Console.Write("カウントする単語を入力: ");
+                word = Console.ReadLine();
+            }
+            WordCounterProcessor._word = word;
+            TextProcessor.Run<WordCounterProcessor>(filePath);
+        }
+    }
+}
---
+++ Chapter15/TemplateMethod/WordCounter/WordCounterProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace WordCounter {
+    public class WordCounterProcessor : TextProcessor {
+        private int _count = 0;
+        public static string _word = "public";
+
+        protected override void Initialize(string fname) =>
+            _count = 0;
+
+        protected override void Execute(string line) {
+            var matches = Regex.Matches(line, _word, RegexOptions.IgnoreCase);
+            foreach (var match in matches) {
+                _count++;
+            }
+        }
+
+        protected override void Terminate() =>
+            Console.WriteLine($"{_count} 個");
+    }
+}
