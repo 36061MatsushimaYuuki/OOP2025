35e6995f2cd6104bcf0bc972b74a9dfea56abd2d
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-11-12 09:21:46 +0900

    演習問題10.4

---
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var di = new DirectoryInfo("./");
+            var files = di.EnumerateFiles("*", SearchOption.AllDirectories)
+                .Where(f => f.Length >= 1048576);
+            foreach (var file in files) {
+                Console.WriteLine(file.Name);
+            }
+        }
+    }
+}
