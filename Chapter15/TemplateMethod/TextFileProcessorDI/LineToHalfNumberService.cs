1bf23a23f437926492249d9685f7630e76d95c34
36061MatsushimaYuuki <oibc36061@ojs.ac.jp>
2025-11-19 09:32:59 +0900

    演習問題15.1完成

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class LineToHalfNumberService : ITextFileService {
+        private int _count = 0;
+
+        public void Initialize(string fname) =>
+            _count = 0;
+
+        public void Execute(string line) {
+            string output = "";
+            foreach (char c in line) {
+                if(c >= '０' && c <= '９') {
+                    output += (char)(c - 0xFEE0);
+                    _count++;
+                } else {
+                    output += c;
+                }
+            }
+            Console.WriteLine(output);
+        }
+
+        public void Terminate() {
+            Console.WriteLine($"(半角への変換数：{_count})");
+        }
+    }
+}
